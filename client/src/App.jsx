7ec8591886900d45cf6205ab7df63284fd63ef5d
fyagci9 <firatyagci50@gmail.com>
2024-11-24 23:01:06 +0300

    24-11-2024

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
+import Categories from "./components/categories/Categories";
+import Header from "./components/header/Header";
 
-import Header from "./components/Header/Header";
 
 function App() {
   return (
-   <div>
-    <Header></Header>
-   </div>
+   <>
+   <Header></Header>
+    <div className="home px-6 flex justify-between gap-10">
+
+      <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-72">
+        <Categories></Categories>
+      </div>
+
+      <div className="product flex-[8]">
+        <div>products</div>
+      </div>
+
+      <div className="carttotals"> 
+        <div>cart totals</div>
+      </div>
+
+    </div>
+
+
+   </>
 
   );
 }
